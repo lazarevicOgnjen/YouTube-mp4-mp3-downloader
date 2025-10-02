ea4cac1cf243d4f9fc526ebfcbbccefd2652e971
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-02 21:35:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ URL = os.getenv("YOUTUBE_URL")
 if not URL:
     sys.exit("❌  YOUTUBE_URL missing")
 
-COOKIES = "youtube.com_cookies.txt"
+COOKIES = os.getenv("YOUTUBE_COOKIES")
+if not COOKIES:
+    sys.exit("❌  YOUTUBE_COOKIES secret missing")
+
+with open("cookies.txt", "w") as f:
+    f.write(COOKIES)
 
 subprocess.run(
-    ["yt-dlp", "--cookies", COOKIES, "-f", "bv*+ba/b", "--no-part", "-o", "video.mp4", URL.strip()],
+    ["yt-dlp", "--cookies", "cookies.txt", "-f", "bv*+ba/b", "--no-part", "-o", "video.mp4", URL.strip()],
     check=True,
 )
 
 subprocess.run(
-    ["yt-dlp", "--cookies", COOKIES, "-x", "--audio-format", "mp3", "--no-part", "-o", "audio.mp3", URL.strip()],
+    ["yt-dlp", "--cookies", "cookies.txt", "-x", "--audio-format", "mp3", "--no-part", "-o", "audio.mp3", URL.strip()],
     check=True,
 )
 
